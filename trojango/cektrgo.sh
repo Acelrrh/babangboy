7f336bc8437170e09db6420157a7f87c5fa8dbca
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-14 02:12:27 +0700

    Update cektrgo.sh

--- trojango/cektrgo.sh
+++ trojango/cektrgo.sh
@@ -61,6 +61,6 @@ oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";
 echo "$oth";
 echo "------------------------------------";
-echo "Script Mod By SL"
+echo "Script Mod By boy"
 rm -rf /tmp/other.txt
 
