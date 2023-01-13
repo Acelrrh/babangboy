ffad71a3ea187f3d51e94445c55e1852872e92c5
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-14 01:55:40 +0700

    Update grpcupdate.sh

--- update/grpcupdate.sh
+++ update/grpcupdate.sh
@@ -6,10 +6,10 @@ cd
 
 cd /usr/bin
 
-wget -O addgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/addgrpc.sh"
-wget -O cekgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/cekgrpc.sh"
-wget -O delgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/delgrpc.sh"
-wget -O renewgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/renewgrpc.sh"
+wget -O addgrpc "https://raw.githubusercontent.com/Acelrrh/babangboy/main/xray/addgrpc.sh"
+wget -O cekgrpc "https://raw.githubusercontent.com/Acelrrh/babangboy/main/xray/cekgrpc.sh"
+wget -O delgrpc "https://raw.githubusercontent.com/Acelrrh/babangboy/main/xray/delgrpc.sh"
+wget -O renewgrpc "https://raw.githubusercontent.com/Acelrrh/babangboy/main/xray/renewgrpc.sh"
 
 
 chmod +x addgrpc
