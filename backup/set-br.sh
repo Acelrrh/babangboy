f07e9610d249326ba73378da6f9c1ca3a4f1353f
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-14 08:31:35 +0700

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+F#!/bin/bash
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/backup"
+akbarvpn="raw.githubusercontent.com/Acelrrh/babangboy/main/backup"
 
 apt install rclone -y
 printf "q\n" | rclone config
